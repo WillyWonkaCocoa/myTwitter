126a5b05a45ea0a4d0d09511046c4b4fdf8ba8c0
William Gao <wgao1@umbc.edu>
2019-11-09 00:33:29 -0500

    Add favorite and unfavorite functions

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -9,9 +9,44 @@
 import UIKit
 
 class TweetCellTableViewCell: UITableViewCell {
+    var favorited:Bool = false
+    var tweetId: Int = -1
+    
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let tobeFavorited = !favorited
+        if (tobeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        }else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        }
+    }
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named:"like-icon-red"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named:"like-icon"), for: UIControl.State.normal)
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
