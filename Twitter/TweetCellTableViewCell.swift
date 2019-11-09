07445b8eefb608ae29d097a691c308df5a7e6a17
William Gao <wgao1@umbc.edu>
2019-11-09 00:42:59 -0500

    Add retweeting

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -20,6 +20,11 @@ class TweetCellTableViewCell: UITableViewCell {
     
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error is retweeting: \(error)")
+        })
     }
     
     @IBAction func favoriteTweet(_ sender: Any) {
@@ -45,6 +50,16 @@ class TweetCellTableViewCell: UITableViewCell {
             favButton.setImage(UIImage(named:"like-icon-red"), for: UIControl.State.normal)
         }else{
             favButton.setImage(UIImage(named:"like-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
         }
     }
     
